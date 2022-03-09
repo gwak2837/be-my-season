4a7ea7e181e2fcf51cf3f97e8b69445c4389cc5e
Taeuk Gwak <gwak2837@kakao.com>
2022-03-09 12:06:30 +0900

    Add auth

--- src/pages/api/program/[id].ts
+++ src/pages/api/program/[id].ts
@@ -1,5 +1,6 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import { isEmptyObject } from 'src/utils'
+import { verifyJWT } from 'src/utils/jwt'
 
 import deleteProgram from './sql/deleteProgram.sql'
 import getBeforeAndAfterProgram from './sql/getBeforeAndAfterProgram.sql'
@@ -10,13 +11,21 @@ import { pool } from '..'
 export default async function handleProgram(req: NextApiRequest, res: NextApiResponse) {
   // Get program
   if (req.method === 'GET') {
+    const jwt = req.headers.authorization
+    let verifiedJwt
+    if (jwt) {
+      verifiedJwt = await verifyJWT(jwt).catch(() => null)
+      if (!verifiedJwt) return res.status(400).send('Invalid JWT')
+    }
+
     const programId = req.query.id
 
     const [rows, rows2] = await Promise.all([
-      pool.query(getProgram, [programId]),
+      pool.query(getProgram, [verifiedJwt?.userId ?? 0, programId]),
       pool.query(getBeforeAndAfterProgram, [programId, programId]),
     ])
 
+    const program = (rows[0] as any)[0]
     const programs = rows2[0] as any
 
     return res.status(200).json({
@@ -27,13 +36,29 @@ export default async function handleProgram(req: NextApiRequest, res: NextApiRes
         : programs[0].id > programId
         ? programs[0]
         : null,
-      program: (rows[0] as any)[0],
+      program: {
+        id: program.id,
+        creationTime: program.creationTime,
+        title: program.title,
+        description: program.description,
+        detail: program.detail,
+        price: program.price,
+        type: program.type,
+        isJoined: Boolean(program.program_id),
+      },
       previousProgram: programs[0]?.id < programId ? programs[0] : null,
     })
   }
 
   // Update program
-  if (req.method === 'PUT') {
+  else if (req.method === 'PUT') {
+    const jwt = req.headers.authorization
+    if (!jwt) return res.status(401).send('Need to authenticate')
+
+    const verifiedJwt = await verifyJWT(jwt).catch(() => null)
+    if (!verifiedJwt) return res.status(400).send('Invalid JWT')
+    if (!verifiedJwt.isAdmin) return res.status(403).send('Need administrator rights')
+
     if (isEmptyObject(req.body)) return res.status(400).send({ message: '값을 입력해주세요.' })
 
     await pool.query(updateProgram, [req.body.description, req.body.detail, req.query.id])
@@ -41,7 +66,14 @@ export default async function handleProgram(req: NextApiRequest, res: NextApiRes
   }
 
   // Delete program
-  if (req.method === 'DELETE') {
+  else if (req.method === 'DELETE') {
+    const jwt = req.headers.authorization
+    if (!jwt) return res.status(401).send('Need to authenticate')
+
+    const verifiedJwt = await verifyJWT(jwt).catch(() => null)
+    if (!verifiedJwt) return res.status(400).send('Invalid JWT')
+    if (!verifiedJwt.isAdmin) return res.status(403).send('Need administrator rights')
+
     const [rows] = await pool.query(deleteProgram, [req.query.id])
     return res.status(200).json({ rows })
   }
